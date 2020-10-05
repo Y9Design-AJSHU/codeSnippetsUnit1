b60f4c462d3e4f705e96a0514405de3f1ce1f6b0
Y9Design-AJSHU <55104759+Y9Design-AJSHU@users.noreply.github.com>
2020-10-05 14:16:20 -0400

    Update save_txt_file_code_snippet.py

--- Code_Snippets/save_txt_file_code_snippet.py
+++ Code_Snippets/save_txt_file_code_snippet.py
@@ -3,7 +3,7 @@ import tkinter.filedialog as tkFileDialog #imports the tkinter.filedialog (the p
 
 def saveas():
 	#this function asks the user to create a name for the file and pick a destination
-	savelocation=tkFileDialog.asksaveasfilename() #assigns the variable savelocation to where the user chose the save the file
+	savelocation=tkFileDialog.asksaveasfilename(defaultextension=".txt") #assigns the variable savelocation to where the user chose the save the file
 	file1=open(savelocation, "w") #opens the saved empty text file to write
 	file1.write("THIS IS WHAT WILL BE IN THE TEXT FILE (WILL BE REPLACED BY LYRICS)") #writes a string (will be lyrics in the actual app)
 	file1.close() #closes the file
